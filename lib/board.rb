c1fbfb0cb377ea4ce788c9124178f2104bb7142b
James Grieve <pocojames@hotmail.com>
2020-08-20 00:34:34 -0700

    add populate method for top and bot of board

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,16 @@
 require "./lib/pawn.rb"
+require "./lib/rook.rb"
+require "./lib/bishop.rb"
+require "./lib/knight.rb"
+require "./lib/queen.rb"
+require "./lib/king.rb"
+
+
+
 class Board
     attr_accessor :board
-    def initialize(player1, player2)
+    def initialize
         @board = Array.new(8) {Array.new(8, " ")}
-        @players = [player1, player2]
     end
 
     def display
@@ -34,18 +41,73 @@ class Board
         end
     end
 
-    def populate #just pawns for now, add other pieces later
-        self.board.each_with_index do |row, y|
+    def populate
+        populate_pawns
+        populate_top
+        populate_bot
+    end
+
+    def populate_pawns #just pawns for now, add other pieces later
+        board = self.board
+        board.each_with_index do |row, y|
             row.each_with_index do |col, x|
-                if y == 1 || y == 6
-                self.board[y][x] = Pawn.new("test")
+                if y == 1 
+                    board[y][x] = Pawn.new("test", "\u265F")
+                elsif y == 6
+                    board[y][x] = Pawn.new("test", "\u2659")
                 end
             end
         end
     end
+
+    def populate_top #populate non-pawn black pieces
+        board = self.board
+        board.each_with_index do |row, y|
+            row.each_with_index do |col, x|
+                if y == 0
+                    if x == 0 || x == 7
+                        board[y][x] = Rook.new("test", "\u265C")
+                    elsif x == 1 || x == 6
+                        board[y][x] = Knight.new("test", "\u265E")
+                    elsif x == 2 || x == 5
+                        board[y][x] = Bishop.new("test", "\u265D")
+                    elsif x == 3
+                        board[y][x] = Queen.new("test", "\u265B")
+                    elsif x == 4
+                        board[y][x] = King.new("test", "\u265A")
+                    end
+                end
+            end
+        end
+    end
+
+    def populate_bot #populate non-pawn white pieces
+        board = self.board
+        board.each_with_index do |row, y|
+            row.each_with_index do |col, x|
+                if y == 7
+                    if x == 0 || x == 7
+                        board[y][x] = Rook.new("test", "\u2656")
+                    elsif x == 1 || x == 6
+                        board[y][x] = Knight.new("test", "\u2658")
+                    elsif x == 2 || x == 5
+                        board[y][x] = Bishop.new("test", "\u2657")
+                    elsif x == 3
+                        board[y][x] = Queen.new("test", "\u2655")
+                    elsif x == 4
+                        board[y][x] = King.new("test", "\u2654")
+                    end
+                end
+            end
+        end
+    end
+
+
+
 end
 
-a = Board.new("A","B")
+a = Board.new
+a.populate_top
 a.populate
 a.display
 
