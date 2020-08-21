882b1331bdc8f4d6681595d179da243ed8291553
James Grieve <pocojames@hotmail.com>
2020-08-20 23:30:04 -0700

    add possible_moves method for knight class

--- lib/knight.rb
+++ lib/knight.rb
@@ -2,11 +2,20 @@ class Knight
     attr_accessor :value
     def initialize(player, value)
         @value = value
+        @piece = "Knight"
         @player = player
     end
 
-    def possible_moves(pos) 
-        y, x = pos
+    def possible_moves(pos)
+        knight_moves = [[1,2],[-1,2],[-1,-2],[1,-2],[2,-1],[2,1],[-2,-1],[-2,1]]
         moves = []
+        y, x = pos
+
+        knight_moves.each do |move|
+            j, i = move
+            moves << [j + y, i + x]
+        end
+        moves
     end
+
 end
