24bf493668899990f28b0eb113ff67e9ccf81de0
James Grieve <pocojames@hotmail.com>
2020-08-21 14:32:46 -0700

    add king possible_moves method to show possible moves

--- lib/king.rb
+++ lib/king.rb
@@ -8,5 +8,38 @@ class King
     def possible_moves(pos) 
         y, x = pos
         moves = []
+        moves << top_moves(pos)
+        moves << side_moves(pos)
+        moves << bot_moves(pos)
+        moves
+    end
+
+    def top_moves(pos)
+        y, x = pos
+        moves = []
+        moves << [y - 1, x]
+        moves << [y - 1, x + 1]
+        moves << [y - 1, x - 1]
+        moves
+    end
+
+    def bot_moves(pos)
+        y, x = pos
+        moves = []
+        moves << [y + 1, x]
+        moves << [y + 1, x + 1]
+        moves << [y + 1, x - 1]
+        moves
+    end
+
+    def side_moves(pos)
+        y, x = pos
+        moves = []
+        moves << [y, x + 1]
+        moves << [y, x - 1]
+        moves
     end
 end
+
+a = King.new("a","b")
+p a.possible_moves ([3,3])
