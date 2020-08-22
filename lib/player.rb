21dfc4851777c54d8e90efb87c5e87d4aebd6c0c
James Grieve <pocojames@hotmail.com>
2020-08-21 17:27:19 -0700

    add player input methods

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,29 @@
 class Player
+    def initialize(player)
+        @player = player
+    end
 
+    def get_input_piece
+        puts "Select a piece:"
+        pos = []
+        puts "Enter y-position:"
+        y_input = gets.chomp.to_i
+        pos << y_input
+        puts "Enter x-position:"
+        x_input = gets.chomp.to_i
+        pos << x_input
+        pos
+    end 
+
+    def get_input_move
+        puts "Move this piece:"
+        pos = []
+        puts "Enter y-position:"
+        y_input = gets.chomp.to_i
+        pos << y_input
+        puts "Enter x-position:"
+        x_input = gets.chomp.to_i
+        pos << x_input
+        pos
+    end
 end
