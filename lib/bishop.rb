fe9626e769fe3a863448741ba33215bf5247d091
James Grieve <pocojames@hotmail.com>
2020-08-21 17:12:03 -0700

    add possible_moves for bishop class, showing all possible moves per position

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -7,6 +7,35 @@ class Bishop
 
     def possible_moves(pos) 
         y, x = pos
+        diagonal_left(pos).each {|move| moves << move}
+        diagonal_right(pos).each {|move| moves << move}
         moves = []
     end
+
+    def diagonal_left(pos)
+        y, x = pos
+        moves = []
+        i = 0
+        while i < 7
+            moves << [y - i, x - i]
+            moves << [y + i, x - i]
+            i += 1
+        end
+        moves        
+    end
+
+    def diagonal_right(pos)
+        y, x = pos
+        moves = []
+        i = 0
+        while i < 7
+            moves << [y - i, x + i]
+            moves << [y + i, x + i]
+            i += 1
+        end
+        moves    
+    end
 end
+
+a = Bishop.new("a","b")
+p a.diagonal_left([3,3])
