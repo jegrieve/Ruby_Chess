96015fab09a89415a310f1811e016dee33e2cf92
James Grieve <pocojames@hotmail.com>
2020-08-21 17:12:29 -0700

    add possible_moves for queen class, showing all possible moves per position

--- lib/queen.rb
+++ lib/queen.rb
@@ -6,7 +6,64 @@ class Queen
     end
 
     def possible_moves(pos) 
+        moves = []
+        horizontal_moves(pos).each {|pos| moves << pos}
+        vertical_moves(pos).each {|pos| moves << pos}
+        diagonal_left(pos).each {|move| moves << move}
+        diagonal_right(pos).each {|move| moves << move}
+        moves
+    end
+
+    def horizontal_moves(pos)
+        moves = []
+        valid_moves = (0..7).to_a
+        y, x = pos
+        x = 0
+        while x < 8
+            move = [y, x]
+            moves << move
+            x += 1
+        end
+        moves
+    end
+
+    def vertical_moves(pos)
+        moves = []
+        valid_moves = (0..7).to_a
+        y, x = pos
+        y = 0
+        while y < 8
+            move = [y, x]
+            moves << move
+            y += 1
+        end
+        moves
+    end
+
+    def diagonal_left(pos)
         y, x = pos
         moves = []
+        i = 0
+        while i < 7
+            moves << [y - i, x - i]
+            moves << [y + i, x - i]
+            i += 1
+        end
+        moves        
+    end
+
+    def diagonal_right(pos)
+        y, x = pos
+        moves = []
+        i = 0
+        while i < 7
+            moves << [y - i, x + i]
+            moves << [y + i, x + i]
+            i += 1
+        end
+        moves    
     end
 end
+
+a = Queen.new("a","b")
+p a.possible_moves([3,3])
