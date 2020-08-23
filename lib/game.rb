0d0e48078e0415820c894e87083d6849fd098b02
James Grieve <pocojames@hotmail.com>
2020-08-23 15:48:47 -0700

    add placement ability to run method

--- lib/game.rb
+++ lib/game.rb
@@ -10,6 +10,7 @@ class Game
     end
 
     def run
+        display_board
         current_player = self.players.first
         piece_input = current_player.get_input_piece
         while !valid_piece?(piece_input, current_player)
@@ -17,8 +18,19 @@ class Game
             piece_input = current_player.get_input_piece
         end
         piece = selected_piece(piece_input)
-        p piece
+        move_input = current_player.get_input_move
+        
+        while !valid_move?(piece_input, move_input, piece)
+            puts "Move not valid, please choose a valid move"
+            move_input = current_player.get_input_move
+        end
+        place_piece(move_input, piece, piece_input)
         self.players.reverse!
+        display_board
+    end
+
+    def display_board
+        self.game_board.display
     end
 
     def valid_piece?(piece_pos, current_player)
@@ -31,6 +43,19 @@ class Game
         y, x = piece_pos
         self.game_board.board[y][x]
     end
+
+    def valid_move?(piece_input, move_input, piece)
+        return true if self.game_board.valid_board_moves.include?(move_input) && piece.possible_moves(piece_input).include?(move_input)
+        false
+    end
+
+    def place_piece(move_input, piece, piece_input)
+        y, x = move_input
+        j, i = piece_input
+        self.game_board.board[y][x] = piece
+        self.game_board.board[j][i] = " "
+    end
+
 
 end
 
