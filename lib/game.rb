232b65bb3e269f5dd41410fdd1997b1193946955
James Grieve <pocojames@hotmail.com>
2020-08-24 22:39:18 -0700

    add valid_moves methodand valid rook moves method

--- lib/game.rb
+++ lib/game.rb
@@ -3,7 +3,7 @@ require "./lib/player.rb"
 
 class Game
     attr_accessor :board, :players, :game_board
-    def initialize(player1,player2)
+    def initialize(player1, player2)
         @game_board = Board.new(player1, player2)
         @players = [player1,player2] #these will be player instances
         run
@@ -20,7 +20,7 @@ class Game
         piece = selected_piece(piece_input)
         move_input = current_player.get_input_move
         
-        while !valid_move?(piece_input, move_input, piece)
+        while !valid_move?(piece_input, move_input, piece) # && valid_game_move(piece_input, move_input, piece)
             puts "Move not valid, please choose a valid move"
             move_input = current_player.get_input_move
         end
@@ -57,7 +57,44 @@ class Game
     end
 
 
+    def valid_game_move(piece_input, move_input, piece)
+        case piece.piece_value
+        when "Rook"
+           return true if rook_valid_moves(piece_input, move_input, piece)
+           false  
+        end
+    end
+
+    def rook_valid_moves(piece_input, move_input, piece)
+        y, x = piece_input #current location input
+        i, j = move_input #move to this location input
+        
+        if y == i #horizontal case
+            move_array = self.game_board.board[y]
+
+            if move_array[j] != " "
+                return false if move_array[j].player == piece.player
+            end
+
+            while x < j 
+                return false if move_array[j] != " "
+                x += 1
+            end
+
+            return true
+
+        elsif x == j #vertical case
+            move_array = self.game_board.board.transpose[x]
+        end
+    end
 end
+
+#Todo:
+#1. Get valid moves for each piece, so we can only moves pieces that aren't "blocked" by other pieces
+#2. Implement check and checkmate feature (for win), after every move, look at the possible moves for each move of the oposing pieces -
+# look at the moves (or look at the valid moves rather) and see if any oposing pieces can reach the position of the king -
+#if it can reach position then that is check (also make sure you can't put yourself in check)
+#if the king has no positions that aren't in check then it is a loss.
 
 Game.new(Player.new("james"),Player.new("adam"))
 
