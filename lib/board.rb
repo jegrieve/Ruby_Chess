c7416362e20992c5e71eb37855a21c8197ea1f8e
James Grieve <pocojames@hotmail.com>
2020-08-28 17:38:55 -0700

    refactor board methods

--- lib/board.rb
+++ lib/board.rb
@@ -8,7 +8,7 @@ class Board
     attr_accessor :board
     def initialize(player1, player2)
         @board = Array.new(8) {Array.new(8, " ")}
-        populate(player1.player, player2.player)
+        populate(player1.id, player2.id)
     end
 
     def display
@@ -45,7 +45,7 @@ class Board
         populate_bot(player1, player2)
     end
 
-    def populate_pawns(player1, player2) #just pawns for now, add other pieces later
+    def populate_pawns(player1, player2)
         board = self.board
         board.each_with_index do |row, y|
             row.each_with_index do |col, x|
@@ -115,32 +115,3 @@ class Board
         end
     end
 end
-
-#white pieces
-# wking = "\u2654"
-# puts wking.encode('utf-8')
-# wqueen = "\u2655"
-# puts wqueen.encode('utf-8')
-# wrook = "\u2656"
-# puts wrook.encode('utf-8')
-# wbishop = "\u2657"
-# puts wbishop.encode('utf-8')
-# wknight = "\u2658"
-# puts wknight.encode('utf-8')
-# wpawn = "\u2659"
-# puts wpawn.encode('utf-8')
-
-#black pieces
-# bking = "\u265A"
-# puts bking.encode('utf-8')
-# bqueen = "\u265B"
-# puts bqueen.encode('utf-8')
-# brook = "\u265C"
-# puts brook.encode('utf-8')
-# bbishop = "\u265D"
-# puts bbishop.encode('utf-8')
-# bknight = "\u265E"
-# puts bknight.encode('utf-8')
-# bpawn = "\u265F"
-# puts bpawn.encode('utf-8')
-
