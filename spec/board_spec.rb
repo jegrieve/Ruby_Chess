9c0b5556408022dcda6579a074df34fcd34f5ade
James Grieve <pocojames@hotmail.com>
2020-09-01 16:20:19 -0700

    add board specs

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,16 @@
 require "./lib/board.rb"
-
+require "./lib/player.rb"
 describe Board do
-    describe "#" do 
-        it "" do
-
+    describe "#initialize" do 
+        it "should create a 8x8 grid" do
+            brd = Board.new(Player.new("Jack"), Player.new("Mike"))
+            count = 0
+            brd.board.each do |row|
+                row.each do |col|
+                    count += 1
+                end
+            end
+            expect(count).to eql(64)
         end
     end
 end
